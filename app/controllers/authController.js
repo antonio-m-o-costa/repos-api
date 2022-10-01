f32563a0a2fd9b2246d7b52b0479a4567d491c81
António Costa <antonio-m-o-costa@users.noreply.github.com>
2022-10-01 17:40:42 +0100

    login and logout routes

--- app/controllers/authController.js
+++ app/controllers/authController.js
@@ -14,49 +14,65 @@ const User = require('../models/userModel');
  * soft deleted can't login
  */
 const login = async (req, res) => {
-    // deny login for soft deleted users
-    try {
-        const user = await User.findOne({
-            username: req.body.username,
-        });
-        if (!user) {
-            logger.error(`invalid username: ${req.body.username}`);
+    if (req.body.username || req.body.password) {
+        try {
+            const user = await User.findOne({
+                username: req.body.username,
+                'deleted.0': { $exists: false },
+            });
+            if (!user) {
+                logger.error(`invalid username: ${req.body.username}`);
+                res.status(400).json({
+                    status: 'error',
+                    message: 'invalid username or password',
+                });
+            } else {
+                logger.info(`pass: ${user.password}`);
+                bcrypt.compare(
+                    req.body.password,
+                    user.password,
+                    async (err, result) => {
+                        if (result) {
+                            req.session.user = {
+                                id: user._id.toString(),
+                                username: user.username,
+                                role: user.role,
+                            };
+                            res.status(200).json({
+                                status: 'success',
+                                message: `user ${user.username} authenticated`,
+                            });
+                        } else {
+                            logger.error(
+                                `invalid password: ${req.body.password}`
+                            );
+                            res.status(400).json({
+                                status: 'error',
+                                message: 'invalid username or password',
+                            });
+                        }
+                    }
+                );
+            }
+        } catch (err) {
+            logger.error(`login failed: ${err}`);
             res.status(400).json({
                 status: 'error',
-                message: 'invalid username provided',
+                message: 'something went very wrong',
+                contact: 'suport@example.com',
             });
-        } else {
-            logger.info(`pass: ${user.password}`);
-            bcrypt.compare(
-                req.body.password,
-                user.password,
-                async (err, result) => {
-                    if (result) {
-                        req.session.user = {
-                            id: user._id.toString(),
-                            username: user.username,
-                            role: user.role,
-                        };
-                        console.log(req.session.user);
-                        res.status(200).json({
-                            status: 'success',
-                            data: user,
-                        });
-                    } else {
-                        logger.error(`invalid password: ${req.body.password}`);
-                        res.status(400).json({
-                            status: 'error',
-                            message: 'invalid password provided',
-                        });
-                    }
-                }
-            );
         }
-    } catch (err) {
-        logger.error(`login failed: ${err}`);
+    } else {
+        logger.error('no login data');
         res.status(400).json({
             status: 'error',
-            message: 'could not login user',
+            message: 'no authentication credentials',
+            expected: {
+                body: {
+                    username: 'username',
+                    password: 'password',
+                },
+            },
         });
     }
 };
@@ -70,13 +86,34 @@ const login = async (req, res) => {
  * maybe with redirect to [/]
  */
 const logout = async (req, res) => {
-    // check if session exists
-    logger.info(`session destroyed: ${req.session}`);
-    req.session.destroy();
-    res.status(200).json({
-        status: 'success',
-        message: 'user logged out',
-    });
+    if (req.session.user) {
+        logger.info(`session destroyed: [${req.session.user.username}]`);
+        res.status(200).json({
+            status: 'success',
+            message: `user [${req.session.user.username}] session destroyed`,
+        });
+        req.session.destroy();
+    } else {
+        logger.error(`no active session`);
+        res.status(400).json({
+            status: 'error',
+            message: 'no active session',
+            options: {
+                home: {
+                    path: 'http://127.0.0.1:3000',
+                    method: 'GET',
+                },
+                login: {
+                    path: 'http://127.0.0.1:3000/auth/login',
+                    method: 'POST',
+                    body: {
+                        username: 'username',
+                        password: 'password',
+                    },
+                },
+            },
+        });
+    }
     // res.redirect('/');
 };
 
