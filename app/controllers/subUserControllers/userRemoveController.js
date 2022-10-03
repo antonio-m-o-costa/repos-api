6d538d5617b144bec5a926a4fc33e8946ca14c5f
António Costa <antonio-m-o-costa@users.noreply.github.com>
2022-10-03 23:41:36 +0100

    user remove sub controller updated
    hard key for admin crated

--- app/controllers/subUserControllers/userRemoveController.js
+++ app/controllers/subUserControllers/userRemoveController.js
@@ -1,5 +1,6 @@
 const url = process.env.SERVER_URL;
 const port = process.env.SERVER_PORT;
+const hardKey = process.env.HARD_KEY;
 
 const logger = require('../../../modules/logger');
 
@@ -16,16 +17,104 @@ const User = require('../../models/userModel');
  * only admin can hard delete users (hard key required)
  */
 const remove = async (req, res, next) => {
-    // if admin check body for hard key
-    // soft delete without hard key
     const id = req.params.id;
-    try {
-        await User.findByIdAndDelete({ _id: id });
-        logger.info(`removed user whit id: ${id}`);
-        res.status(200).json({ status: 'success', data: id });
-    } catch (err) {
-        logger.error(`deleting user error: ${err}`);
-        res.status(400).json({ status: 'error', message: 'user not found' });
+    const activeUser = req.session.user;
+    const userToDelete = await User.findOne({
+        _id: id,
+        'deleted.at': { $exists: false },
+    });
+    console.log(userToDelete);
+    if (userToDelete == null) {
+        logger.error(`user ${id} not found`);
+        return res.status(500).send({
+            status: 'error',
+            message: `user ${id} not found`,
+        });
+    }
+    logger.info(`user to delete ${userToDelete}`);
+    switch (activeUser.role) {
+        case 'user':
+            if (activeUser.id != userToDelete._id) {
+                res.status(400).json({
+                    status: 'error',
+                    message: 'you can only delete yourself',
+                });
+            } else {
+                updateUser();
+            }
+            break;
+        case 'mod':
+            if (userToDelete.role == 'admin') {
+                res.status(400).json({
+                    status: 'error',
+                    message: 'you can not delete admin users',
+                });
+            } else {
+                updateUser();
+            }
+            break;
+        case 'admin':
+            console.log(req.body.key == hardKey);
+            if (req.body.key == hardKey) {
+                const hardDelete = User.findOneAndDelete(id, (err, result) => {
+                    if (err) {
+                        logger.error(`deleting error ${err}`);
+                        res.status(400).json({
+                            status: 'error',
+                            message: 'user could not be deleted',
+                        });
+                    } else if (result) {
+                        res.status(200).json({
+                            status: 'success',
+                            message: `user [${hardDelete.username}] deleted`,
+                        });
+                    }
+                });
+            } else {
+                updateUser();
+            }
+            break;
+        default:
+            res.status(400).json({
+                status: 'error',
+                message: 'something went very wrong',
+                contact: 'suport@example.com',
+            });
+            break;
+    }
+    function updateUser() {
+        const deletedUser = User.findOneAndUpdate(
+            { _id: id },
+            {
+                $set: {
+                    deleted: {
+                        at: Date.now(),
+                        by: activeUser.id,
+                    },
+                },
+            },
+            (err, result) => {
+                if (err) {
+                    logger.error(`deleting user error ${err}`);
+                    res.status(400).json({
+                        status: 'error',
+                        message: 'user could not be deleted',
+                    });
+                } else if (result) {
+                    logger.info(`deleted user to ${deletedUser}`);
+                    res.status(200).json({
+                        status: 'success',
+                        message: `user deleted [${result.username}] successfully`,
+                        options: {
+                            users: {
+                                path: `${url}:${port}/users`,
+                                method: 'GET',
+                            },
+                        },
+                    });
+                }
+            }
+        );
     }
 };
 
